690c0fd2fa88c11eb13e53eda16e95092bdf87ed
Gregory Vigo Torres <>
2018-08-12 11:55:34 +0200

    spelt item

--- spelt/items.py
+++ spelt/items.py
@@ -1,14 +1,9 @@
 # -*- coding: utf-8 -*-
 
-# Define here the models for your scraped items
-#
-# See documentation in:
-# http://doc.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
 
-class IrizarItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class SpeltItem(scrapy.Item):
+    url = scrapy.Field()
+    document = scrapy.Field()
+    encoding = scrapy.Field()
